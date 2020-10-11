2062266591e816f163e878bc0ee10f265d96e931
neeleshjaiswal <neelesh.jaiswal08@gmail.com>
2020-10-11 12:52:38 +0530

    feat: initial commit

---
+++ src/com/neel/datastructure/ClassRunner.java
@@ -0,0 +1,11 @@
+package com.neel.datastructure;
+
+public class ClassRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Neel");
+
+	}
+
+}
